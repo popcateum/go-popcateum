df60cecaee06cf9535539b0ea229a2817d976242
popcateum <popcateum@gmail.com>
2021-05-26 09:22:06 +0900

    consensus/misc: fix forks.go

--- consensus/misc/forks.go
+++ consensus/misc/forks.go
@@ -17,9 +17,6 @@
 package misc
 
 import (
-	"fmt"
-
-	"github.com/popcateum/go-popcateum/common"
 	"github.com/popcateum/go-popcateum/core/types"
 	"github.com/popcateum/go-popcateum/params"
 )
@@ -34,9 +31,9 @@ func VerifyForkHashes(config *params.ChainConfig, header *types.Header, uncle bo
 	}
 	// If the homestead reprice hash is set, validate it
 	if config.EIP150Block != nil && config.EIP150Block.Cmp(header.Number) == 0 {
-		if config.EIP150Hash != (common.Hash{}) && config.EIP150Hash != header.Hash() {
-			return fmt.Errorf("homestead gas reprice fork: have 0x%x, want 0x%x", header.Hash(), config.EIP150Hash)
-		}
+		// if config.EIP150Hash != (common.Hash{}) && config.EIP150Hash != header.Hash() {
+		// 	return fmt.Errorf("homestead gas reprice fork: have 0x%x, want 0x%x", header.Hash(), config.EIP150Hash)
+		// }
 	}
 	// All ok, return
 	return nil
